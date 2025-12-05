582aac1907c155b5e04dd9a962718a4bdf890f93
devamanas-t <devamanastheneri@gmail.com>
2025-12-05 16:48:41 +0530

    Login page 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import LandingPage from './pages/LandingPage'
+import Login from './pages/Login'
+
+
+
 
 const App = () => {
   return (
     <div>
       <LandingPage />
+      <Login />
     </div>
   )
 }
